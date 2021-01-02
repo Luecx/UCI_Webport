7c1a01ae61038fbf6f1e9076d311e2023d22d691
Finn Eggers <web@finneggers.de>
2021-01-02 17:22:46 +0100

    small style changes

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -1,5 +1,34 @@
-
 $(document).ready(function () {
+
+
+    $("#slider").slider({
+        value: 4,
+        min: 1,
+        max: 7,
+        step: 1
+    }).each(function() {
+
+            // Add labels to slider whose values
+            // are specified by min, max
+
+            // Get the options for this slider (specified above)
+            var opt = $(this).data().uiSlider.options;
+
+            // Get the number of possible values
+            var vals = opt.max - opt.min;
+
+            // Position the labels
+            for (var i = 0; i <= vals; i++) {
+
+                // Create a new element and position it with percentages
+                var el = $('<label>' + (i + opt.min) + '</label>').css('left', (i/vals*100) + '%');
+
+                // Add the element inside #slider
+                $("#slider").append(el);
+
+            }
+
+        });
 
     w = $(".sidepanel-box").width();
     if (w<300) {
@@ -8,6 +37,8 @@ $(document).ready(function () {
             $(this).addClass("col-6");
         });
     }
+
+
 
     $(window).resize(function () {
         w = $(".sidepanel-box").width();
